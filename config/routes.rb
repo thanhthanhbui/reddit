cb21ac3a0a36487f53f07020be674fc7bdb7d1e0
Thanh Bui <ttthanhbui@gmail.com>
2019-02-20 10:28:13 -0700

    add subs and topics routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "subs#index"
+
+  # resources :controller
+  resources :subs do
+    resources :topics
+  end
+  # http - url - controller#action
+  # get "/people", to: "people#index"
+  # get "/people/:id", to: "people#show"
+  # delete "/people/:id", to: "people#dstroy"
 end
+
